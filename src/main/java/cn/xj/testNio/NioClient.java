64612b3cbfc58ba6fb1a8e783b7cae618360947e
794405085 <794405085@qq.com>
2020-02-13 22:28:35 +0800

    nio网络编程2

--- src/main/java/cn/xj/testNio/NioClient.java
+++ src/main/java/cn/xj/testNio/NioClient.java
@@ -1,11 +1,39 @@
 package cn.xj.testNio;
 
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
 /**
  * @ClassName NioClient
- * @Description:
+ * @Description:Nio网络编程客户端
  * @Author 79440
  * @Date 2020/2/13
  * @Version V1.0
  **/
 public class NioClient {
+    @Test
+    public void nioClientTest() throws Exception{
+
+        //1.得到一个网络通道
+        SocketChannel socketChannel = SocketChannel.open();
+        //2.设置非阻塞方式
+        socketChannel.configureBlocking(false);
+        //3.提供服务端的地址和端口号
+        InetSocketAddress address = new InetSocketAddress("127.0.0.1", 9999);
+        //4.连接到服务器
+        if (!socketChannel.connect(address)){
+            while (!socketChannel.finishConnect()){
+                System.out.println("客户端没有连接到服务端 在干别的事");
+            }
+        }
+        //5.得到一个缓冲区并写入数据
+        String str = "hello NioServer";
+        ByteBuffer bytes = ByteBuffer.wrap(str.getBytes());
+        //6.发送数据
+        socketChannel.write(bytes);
+         System.in.read();
+    }
 }
--- src/main/java/cn/xj/testNio/NioServer.java
+++ src/main/java/cn/xj/testNio/NioServer.java
@@ -3,9 +3,9 @@ package cn.xj.testNio;
 import org.junit.Test;
 
 import java.net.InetSocketAddress;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
 
 /**
  * @ClassName NioServer
@@ -30,7 +30,34 @@ public class NioServer {
         ssChannel.register(selector, SelectionKey.OP_ACCEPT);
         //6.doWork
         while (true){
+            //6.1监控客户端
+            if (selector.select(2000)==0){
+                System.out.println("Server:没有客户端搭理我，我就干点别的事");
+                continue;
+            }
+            //6.2得到SelectionKey,判断通道里的事件
+            Iterator<SelectionKey> keyIterators = selector.selectedKeys().iterator();
+            while (keyIterators.hasNext()){
+                SelectionKey key = keyIterators.next();
 
+                if (key.isAcceptable()){//客户端连接事件
+                    System.out.println("OP_ACCEPT");
+                    SocketChannel accept = ssChannel.accept();//通过ServerSocketChannel获得channel
+                    accept.configureBlocking(false);//非阻塞方式
+                    accept.register(selector,SelectionKey.OP_READ, ByteBuffer.allocate(1024));//将通道注册到selector
+                }
+                if (key.isReadable()){//读取客户端发送的消息
+                    System.out.println("OP_READ");
+                    SocketChannel channel = (SocketChannel)key.channel();//通过ServerSocketChannel获得channel
+                    ByteBuffer buffer = (ByteBuffer)key.attachment();
+                    channel.read(buffer);
+                    System.out.println("客户端发过来的消息"+new String(buffer.array()));
+                }
+
+
+            }
+            //手动从集合中移除当前key，防止重复处理
+            keyIterators.remove();
         }
 
     }
